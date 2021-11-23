1f48c529f12b4298dab4d3865bcce13af5bf8026
kwangsun <eyeyel@naver.com>
2021-11-24 00:11:08 +0900

    평점입력 작업중

--- src/components/DescPanel.tsx
+++ src/components/DescPanel.tsx
@@ -7,10 +7,11 @@ interface Props {
     title: string
     summary?: string
     desc?: string
+    showSpread?: boolean
     children?: JSX.Element
 }
 
-function DescPanel({title, summary='', desc='', children}: Props) {
+function DescPanel({title, summary='', desc='', showSpread=false, children}: Props) {
     const [spread, setSpread] = useState(false);
 
     const onClickSpread = ()=>{
@@ -21,10 +22,10 @@ function DescPanel({title, summary='', desc='', children}: Props) {
         <div css={style(spread)}>
             <div className='title'>{title}</div>
             <div className='content'>
-                {summary}<br/><br/>
+                {summary ? <>{summary} <br/><br/></> : null}
                 {desc}
                 {children}
-                <div className='spread' onClick={onClickSpread}>펼쳐보기</div>
+                {showSpread ? <div className='spread' onClick={onClickSpread}>{spread ? '접기' : '펼쳐보기'}</div> : null}
             </div>
         </div>
     );
@@ -43,13 +44,31 @@ const style = (spread: boolean)=> (css`
     }
     .content {
         width: 100%;
-        ${spread ? 'height: 100%;' : 'height: 180px;'}
+        min-height: 180px;
+        // ${spread ? '' : 'height: 180px;'}
         font-size: 13px;
         color: var(--gray_80);
-        padding: 20px 0 0 0;
+        padding: 12px 0 0 0;
         line-height: 22px;
         .spread {
-
+            font-size: 13px;
+            color: var(--dodgeblue_60);
+            text-align: right;
+            margin-right: 15px;
+            cursor: pointer;
+            &::after {
+                position: absolute;
+                content: '^';
+                font-weight: bold;
+                color: white;
+                text-align: center;
+                line-height: 17px;
+                background-color: var(--dodgeblue_60);
+                width: 13px;
+                height: 13px;
+                border-radius: 50%;
+                transform: translate(2px, 4px) rotate(${spread ? '0' : '180deg'});
+            }
         }
     }
 `);
